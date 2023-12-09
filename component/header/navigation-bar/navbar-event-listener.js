48b22d6d54d81e0c7d8f16db3ed17b88e7a04e84
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-09 23:51:17 +0100

    Continue with the conditions

--- component/header/navigation-bar/navbar-event-listener.js
+++ component/header/navigation-bar/navbar-event-listener.js
@@ -5,6 +5,10 @@ function handleNavbarClick(event) {
         initializeContent(pages[0].pageTemplate())
     } else if (event.target.id === 'nav-classes') {
         initializeContent(pages[1].pageTemplate)
+    } else if (event.target.id === 'nav-teachers') {
+        initializeContent(pages[2].pageTemplate)
+    } else if (event.target.id === 'nav-students') {
+        initializeContent(pages[3].pageTemplate)
     }
 }
 let navbarListenet = document.getElementById('main-container').addEventListener('click', handleNavbarClick);

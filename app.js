11e4d79b004541beb9dadeafa2590af804b3b26f
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-09 13:19:13 +0100

    call the imported functions and check one of them by using 'console.log'

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 import { saveClassesToLocalStorage, getClassesFromLocalStorage, saveStudentsToLocalStorage, getStudentsFromLocalStorage, saveTeachersToLocalStorage, getTeachersFromLocalStorage } from './local-storage/data-importer.js'
+saveClassesToLocalStorage();
+saveStudentsToLocalStorage();
+saveTeachersToLocalStorage();
+getClassesFromLocalStorage();
+getStudentsFromLocalStorage();
+getTeachersFromLocalStorage();
+console.log(`class data`, getClassesFromLocalStorage())
 document.addEventListener('DOMContentLoaded', function () {
 
 });

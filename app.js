267aa0d5670cdc5d413bd19d5fb3df098b1de381
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-09 13:15:09 +0100

    Import all the exported functions from data-importer to 'app.js'

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import { saveClassesToLocalStorage, getClassesFromLocalStorage, saveStudentsToLocalStorage, getStudentsFromLocalStorage, saveTeachersToLocalStorage, getTeachersFromLocalStorage } from './local-storage/data-importer.js'
 document.addEventListener('DOMContentLoaded', function () {
 
 });

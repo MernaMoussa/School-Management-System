2e8f1d93313366fa0a701d082afbe2748a42f648
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-09 12:58:33 +0100

    Create 2 functions 'saveClassesToLocalStorage' to save the classes data, and 'getClassesFromLocalStorage' to retrieve them, and export both to use them in 'app.js'

--- local-storage/data-importer.js
+++ local-storage/data-importer.js
@@ -2,3 +2,15 @@ import classesData from '../school-data/classes-data.js';
 import studentsData from '../school-data/students-data.js';
 import teachersData from '../school-data/teachers-data.js';
 import { saveDataToLocalStorage, getDataFromLocalStorage } from './local-storage-handler.js';
+function saveClassesToLocalStorage() {
+    saveDataToLocalStorage('classesData', classesData);
+}
+
+function getClassesFromLocalStorage() {
+    return getDataFromLocalStorage('classesData');
+}
+
+export {
+    saveClassesToLocalStorage,
+    getClassesFromLocalStorage,
+};

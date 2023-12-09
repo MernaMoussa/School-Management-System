4bbd7dd57c7cb7a179f118dc9c09c20c81ba278b
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-09 12:52:10 +0100

    Create new Js file 'data-importer.js' to import the school data in it to call them in the local storage functions

---
+++ local-storage/data-importer.js
@@ -0,0 +1,4 @@
+import classesData from '../school-data/classes-data.js';
+import studentsData from '../school-data/students-data.js';
+import teachersData from '../school-data/teachers-data.js';
+import { saveDataToLocalStorage, getDataFromLocalStorage } from './local-storage-handler.js';

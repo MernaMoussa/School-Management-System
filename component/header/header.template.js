d86d56fc55a81c8ea7a467a6d56012090fcefa06
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-09 22:10:32 +0100

    Adjust headerTemplate readablity and fix the problem with the button nav in small window

--- component/header/header.template.js
+++ component/header/header.template.js
@@ -1,23 +1,27 @@
 import { generateNavItems } from "./navigation-bar/navbar.module.js"
 function headerTemplate(pages) {
     return `
-          <nav id="navbar" class="navbar navbar-expand-md bg-dark navbar-dark p-4">
-              <a class="navbar-brand" href="#">LMS</a>
-              <button
+        <nav id="navbar" class="navbar navbar-expand-lg navbar-dark">
+          <div class="container">
+                <a class="navbar-brand" href="#">LMS</a>
+                <button
                   class="navbar-toggler"
                   type="button"
-                  data-toggle="collapse"
-                  data-target="#collapsibleNavbar"
-              >
-                  <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
-                  <ul class="navbar-nav">
-                  ${generateNavItems(pages)}
-                  </ul>
-              </div>
-              <i class="bi bi-person-circle fs-3 text-white"></i>
-          </nav>
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapsibleNavbar"
+                >
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-center" id="collapsibleNavbar">
+                    <ul class="navbar-nav">
+                    ${generateNavItems(pages)}
+                    </ul>
+                </div>
+                <div class="d-flex align-items-center">
+                    <i class="bi bi-person-circle fs-3 text-white mr-3"></i>
+                </div>
+            </div>
+        </nav>
       `;
 }
 export default headerTemplate
--- component/main-template/main.template.js
+++ component/main-template/main.template.js
@@ -1,7 +1,7 @@
 export function mainTemplate() {
     return `
        <div class="main-wrapper ">
-           <div class="header-section"> </div> 
+       <div class="header-section bg-dark text-light py-3"> </div> 
            <div class="content-section container-md py-5"> </div>
            <div class="footer-section bg-dark text-light py-3"> </div>
        </div>

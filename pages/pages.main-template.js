4b9d76c0a6fe1959045bc8768d4b8884b230004f
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-10 12:48:32 +0100

    Create button to add new data in the storage

--- pages/pages.main-template.js
+++ pages/pages.main-template.js
@@ -2,6 +2,7 @@ export function initializeMainTemplate(initializePagesFunction, pageHeader) {
     return `
         <div class="row justify-content-center">
             <h1>${pageHeader}</h1>
+            <button type="button" class="btn btn-info">Add New ${pageHeader}</button>
             ${initializePagesFunction}
         </div>`
 }

546bd4a2326ee566e911269bb8b6493e92a9f5d6
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-10 14:15:29 +0100

    Style Add button

--- pages/pages.main-template.js
+++ pages/pages.main-template.js
@@ -2,7 +2,11 @@ export function initializeMainTemplate(initializePagesFunction, pageHeader) {
     return `
         <div class="row justify-content-center">
             <h1>${pageHeader}</h1>
-            <button type="button" class="btn btn-info">Add New ${pageHeader}</button>
+            <div class="col-12">
+                <div class="d-flex justify-content-end">
+                    <button type="button" class="btn btn-info col-2">Add New ${pageHeader}</button>
+                </div>
+            </div>
             ${initializePagesFunction}
         </div>`
 }

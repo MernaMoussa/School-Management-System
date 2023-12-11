dd91f606e5d572e07e7a21e268167aff252a9ba2
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-11 21:42:52 +0100

    Modularize: homeTemplate

--- pages/home-page/home.template.js
+++ pages/home-page/home.template.js
@@ -1,33 +1,23 @@
-function homeTemplate(countedClasses, countedStudents, countedTeachers) {
+export function homeTemplate(countedClasses, countedStudents, countedTeachers) {
     return `
-    <h1 class="mb-4"> Welcome Merna</h1>
+        <h1 class="mb-4"> Welcome Merna</h1>
         <div class="row row-cols-1 row-cols-md-3 g-4 text-center">
-            <div class="col-12">
-                <div class="container card h-100 border-dark col-8">
-                    <div class="card-body">
-                        <h4 class="card-title">${countedStudents}</h4>
-                        <p class="card-text">Number of students</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="container card h-100 border-dark col-8">
-                    <div class="card-body">
-                        <h4 class="card-title">${countedTeachers}</h4>
-                        <p class="card-text">Number of teachers</p>
-                    </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="container card h-100 border-dark col-8">
-                    <div class="card-body">
-                        <h4 class="card-title">${countedClasses}</h4>
-                        <p class="card-text">Number of classes</p>
-                    </div>
+            ${generateHomeCard(countedStudents, "Number of students")}
+            ${generateHomeCard(countedTeachers, "Number of teachers")}
+            ${generateHomeCard(countedClasses, "Number of classes")}
+        </div>
+    `;
+}
+
+function generateHomeCard(countedData, description) {
+    return `
+        <div class="col-12">
+            <div class="container card h-100 border-dark col-8">
+                <div class="card-body">
+                    <h4 class="card-title">${countedData}</h4>
+                    <p class="card-text">${description}</p>
                 </div>
             </div>
         </div>
-      `;
+    `;
 }
-
-export { homeTemplate };

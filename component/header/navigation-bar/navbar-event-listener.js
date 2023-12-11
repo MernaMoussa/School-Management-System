d9a9755c7b8802942b2c8f85a15474fc0d79e318
Merna Moussa <mernahelmy.m@gmail.com>
2023-12-11 22:09:12 +0100

    Modularize : handleNabbarClick

--- component/header/navigation-bar/navbar-event-listener.js
+++ component/header/navigation-bar/navbar-event-listener.js
@@ -1,24 +1,15 @@
 import { initializeContent } from "../../content/content.module.js";
+
 export function handleNavbarClick(pages, storedClasses, storedStudents, storedTeachers) {
     console.log(event);
     const pageId = event.target.id;
     const selectedPage = pages.find(page => `nav-${page.pageName.toLowerCase()}` === pageId);
     console.log(selectedPage);
-    switch (selectedPage.pageName) {
-        case 'Home':
-            initializeContent(pages, selectedPage.pageTemplate(storedClasses, storedStudents, storedTeachers));
-            break;
-        case 'Classes':
-            initializeContent(pages, selectedPage.pageTemplate(storedClasses, storedStudents, storedTeachers));
-            break;
-        case 'Students':
-            initializeContent(pages, selectedPage.pageTemplate(storedClasses, storedStudents, storedTeachers));
-            break;
-        case 'Teachers':
-            initializeContent(pages, selectedPage.pageTemplate(storedClasses, storedStudents, storedTeachers));
-            break;
-        default:
-            console.error(`Page type not valid: ${selectedPage.pageName}`);
+
+    if (selectedPage) {
+        initializeContent(pages, selectedPage.pageTemplate(storedClasses, storedStudents, storedTeachers));
+    } else {
+        console.error(`Page type not valid: ${selectedPage ? selectedPage.pageName : 'unknown'}`);
     }
 }
 
